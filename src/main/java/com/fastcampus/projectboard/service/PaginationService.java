723a432e08050c77fdace7dbfcd4537aaae416d7
TIKITAK <gusxkr4816@naver.com>
2024-05-06 22:57:22 +0900

    20240506 학습내용 commit
    20240506 학습내용 commit

--- src/main/java/com/fastcampus/projectboard/service/PaginationService.java
+++ src/main/java/com/fastcampus/projectboard/service/PaginationService.java
@@ -1,2 +1,22 @@
-package com.fastcampus.projectboard.service;public class PaginationService {
+package com.fastcampus.projectboard.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaginationService {
+
+    //페이징은 고정적으로 5개 가지도록 변수 선언하였습니다.
+    private static final int BAR_LENGTH = 5;
+
+    //현재 페이지 번호와 전체 페이지를 가져와줍니다.
+    public List<Integer> getPaginationBarNumbers(int currentPageNumber, int totalPages) {
+        return null;
+    }
+
+    //페이징바 길이 조회 가능하도록 하기 위해 생성하였습니다.
+    public int currentBarLength() {
+        return BAR_LENGTH;
+    }
 }
