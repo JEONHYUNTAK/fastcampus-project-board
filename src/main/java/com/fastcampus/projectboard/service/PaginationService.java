f1457c17872f8591c2ca7d35f94406c21a8aba43
TIKITAK <gusxkr4816@naver.com>
2024-05-26 18:42:06 +0900

    #23 - 페이지네이션 구현 및 테스트
    스프링 서비스 빈 형태로 설계하여
    현재 페이지와 총 페이지 수를 넣어주면
    페이지 바에 사용할 번호 리스트를 반환하게끔 설계

--- src/main/java/com/fastcampus/projectboard/service/PaginationService.java
+++ src/main/java/com/fastcampus/projectboard/service/PaginationService.java
@@ -3,6 +3,7 @@ package com.fastcampus.projectboard.service;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class PaginationService {
@@ -12,7 +13,10 @@ public class PaginationService {
 
     //현재 페이지 번호와 전체 페이지를 가져와줍니다.
     public List<Integer> getPaginationBarNumbers(int currentPageNumber, int totalPages) {
-        return null;
+        int startNumber = Math.max(currentPageNumber - (BAR_LENGTH / 2), 0);
+        int endNumber = Math.min(startNumber + BAR_LENGTH, totalPages);
+
+        return IntStream.range(startNumber, endNumber).boxed().toList();
     }
 
     //페이징바 길이 조회 가능하도록 하기 위해 생성하였습니다.
